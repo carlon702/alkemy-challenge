bf3b568b102939d048131fbd5143d621ad4344c6
carlon702 <carlosj.marchal@gmail.com>
2023-01-24 20:51:11 -0300

    First commit Alkemy Challenge

---
+++ src/elements/Login.js
@@ -0,0 +1,127 @@
+import axios from "axios"
+import sweetAlert from 'sweetalert2';
+
+
+
+
+
+function Login() {
+
+
+    
+    
+
+    const SubmitHandler = e => {
+
+
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+       
+      
+       
+       
+        
+        
+       
+
+        const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+
+        if(email===""||password===""){
+            sweetAlert.fire({
+                title: 'Los campos no pueden estar vacios!',
+                text: 'Llena los campos necesarios para continuar',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+              }) ;
+            return;//return stops validations if triggered
+        }
+
+        if(email !== "" && !regexEmail.test(email)){
+            sweetAlert.fire({
+                title: 'Debes introducir un email valido',
+                text: 'El formato de email debe ser xxx@xxx.xxx',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            });
+            return;
+        }
+
+        if(email !== "challenge@alkemy.org" || password !== "react"){
+            sweetAlert.fire({
+                title: 'Credenciales invalidas',
+                text: 'El email y/o contrasena son incorrectos',
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            });
+            return
+        }
+
+
+        
+        axios.post('http://challenge-react.alkemy.org', {email, password})
+        .then(response => {
+            sweetAlert.fire({
+                title: "Exito",
+                text: 'Logueado con exito',
+                icon: 'success',
+                confirmButtonText: 'Ok'
+            })
+            const token = response.data.token;
+            
+            localStorage.setItem('token', token);
+            if(localStorage.token){
+                setTimeout(function () {
+                    window.location.href = "/list";
+                 }, 3000);
+                 
+            }
+            
+            
+        })
+        
+    
+     
+            
+        
+
+        
+        
+            
+            
+              
+         
+        }
+
+      
+        
+    return (
+        <div>
+            
+        
+        <form onSubmit={SubmitHandler}>
+            
+            <label>
+                <span>Correo Electronico</span> <br/>
+            <input type="text" name="email" />
+            </label>
+            <br/>
+            <label>
+                <span>Contrasena</span> <br/>
+            <input type="password" name="password" />
+            </label>
+            <br/>
+            <button type="submit"> Ingresar </button>
+
+        </form>
+
+        </div>
+    )
+};
+
+
+
+export default Login
+
+
